9d38a6c759b6bd5de920724c4d8174b1d19465a5
Kaushik Biswas <kbutsho@gmail.com>
2023-08-07 00:14:38 +0600

    UPDATE

--- src/components/Home/Brand.js
+++ src/components/Home/Brand.js
@@ -5,7 +5,7 @@ const Brand = (props) => {
     return (
         <div className={`${styles.brand_card}`}>
             <div className="text-center">
-                <Image className={`mt-2 ${styles.brand_image}`} src={coverPhoto} height={60} width={115} layout='responsive' alt="image" />
+                <Image className={`mt-2 ${styles.brand_image}`} src={coverPhoto} height={55} width={105}  alt="image" />
             </div>
         </div>
     );
