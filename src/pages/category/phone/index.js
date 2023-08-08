af9f605a266293707aca720fbd4367a647088158
Kaushik Biswas <kbutsho@gmail.com>
2023-08-08 19:38:40 +0600

    update

--- src/pages/category/phone/index.js
+++ src/pages/category/phone/index.js
@@ -6,23 +6,42 @@ import ProductCard from '@/components/Category/ProductCard';
 import Slider from '@mui/material/Slider';
 import { useState } from 'react';
 import styles from '@/styles/home/product.module.css'
-import { IoIosArrowDropup, IoIosArrowDropdown } from 'react-icons/io'
+import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io'
+import { PRODUCT_STATUS } from '@/constant/product.constant';
 
 const PhoneCategory = ({ data }) => {
     const [product, setProduct] = useState(data.productResponse);
+
+    // filter by price range
     const [priceRange, setPriceRange] = useState(data.priceRangeResponse.data);
     const handlePriceRangeChange = (event, newValue) => {
         setPriceRange(newValue);
     }
+    const [priceToggle, setPriceToggle] = useState(true)
+    const handelPriceToggle = () => {
+        setPriceToggle(!priceToggle)
+    }
+
+    // filter by status
+    const productStatus = [PRODUCT_STATUS.IN_STOCK, PRODUCT_STATUS.LIMITED_STOCK, PRODUCT_STATUS.UPCOMING, PRODUCT_STATUS.STOCK_OUT, PRODUCT_STATUS.DISCONTINUE]
+    const [filterByStatus, setFilterByStatus] = useState('');
+    const handelFilterByStatus = (event) => {
+        setFilterByStatus(event.target.value);
+    }
+
+    const [statusToggle, seStatusToggle] = useState(true)
+    const handelStatusToggle = () => {
+        seStatusToggle(!statusToggle)
+    }
+
+
+
+    // filter 
     const filteredData = product.data.filter((p) => {
         const range = p.price >= priceRange[0] && p.price <= priceRange[1];
         return range;
     });
 
-    const [priceToggle, setPriceToggle] = useState(false)
-    const handelPriceToggle = () => {
-        setPriceToggle(!priceToggle)
-    }
     return (
         <div className='container'>
             <Breadcrumb />
@@ -30,27 +49,57 @@ const PhoneCategory = ({ data }) => {
                 <Image src={banner} layout='responsive' height={360} width={1920} alt="img" />
                 <div className="phone-area py-4">
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className="col-md-3 fixed">
                             <div className={styles.filter_header} style={{ marginTop: "12px" }}>
                                 <span>Filter</span>
                             </div>
-                            <div className={styles.priceRange_area}>
-                                <button className='fw-bold d-flex justify-content-between' onClick={handelPriceToggle}>Price Range
+
+                            {/* filter by price range */}
+                            <div className={styles.filter_area}>
+                                <button onClick={handelPriceToggle}>Price Range
                                     <div>
-                                        <span className={` ${priceToggle ? styles.show_slider : styles.hide_slider}`}><IoIosArrowDropup size="20px" /></span>
-                                        <span className={` ${priceToggle ? styles.hide_slider : styles.show_slider}`}><IoIosArrowDropdown size="20px" /></span>
+                                        <span className={` ${priceToggle ? styles.show : styles.hide}`}><IoIosArrowUp size="20px" /></span>
+                                        <span className={` ${priceToggle ? styles.hide : styles.show}`}><IoIosArrowDown size="20px" /></span>
                                     </div>
                                 </button>
 
-                                <div className={`mt-3 px-2 ${priceToggle ? styles.show_slider : styles.hide_slider}`}>
+                                <div className={`mt-3 px-2 ${priceToggle ? styles.show : styles.hide}`}>
+                                    <hr />
                                     <Slider value={priceRange} onChange={handlePriceRangeChange} valueLabelDisplay="auto" min={data.priceRangeResponse.data[0]} max={data.priceRangeResponse.data[1]} />
-                                    <div className='d-flex justify-content-between'>
+                                    <div className='d-flex justify-content-between pb-2'>
                                         <small>{priceRange[0]}</small>
                                         <small>{priceRange[1]}</small>
                                     </div>
                                 </div>
                             </div>
 
+                            {/* // filter by status */}
+                            <div className={styles.filter_area}>
+                                <button onClick={handelStatusToggle}>Status
+                                    <div>
+                                        <span className={` ${statusToggle ? styles.show : styles.hide}`}><IoIosArrowUp size="20px" /></span>
+                                        <span className={` ${statusToggle ? styles.hide : styles.show}`}><IoIosArrowDown size="20px" /></span>
+                                    </div>
+                                </button>
+
+                                <div className={`mt-3 px-2 ${statusToggle ? styles.show : styles.hide}`}>
+                                    <hr />
+                                    {
+                                        productStatus.map((status, index) => (
+                                            <div className={styles.radio_area} key={index}>
+                                                <input
+                                                    type="radio"
+                                                    name="status"
+                                                    id={`status_${index}`}
+                                                    value={status}
+                                                    onChange={handelFilterByStatus}
+                                                />
+                                                <label htmlFor={`status_${index}`}>{status.split('-').join(' ')}</label>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
                         </div>
                         <div className="col-md-9">
                             <div className="row">
