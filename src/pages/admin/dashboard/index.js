d118ddc3a7369294b37014fe6898b466c9fe4133
Kaushik Biswas <kbutsho@gmail.com>
2023-08-20 18:19:47 +0600

    ashboard margin fix

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -11,22 +11,22 @@ const Dashboard = () => {
             <Breadcrumb />
             <div className="p-md-5 p-2">
                 <div className="row">
-                    <div className="col-md-3 my-2">
+                    <div className="col-md-3 mb-2">
                         <div className="bg-info p-3">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur quia dolor temporibus expedita. Amet, minima dolor. Rem repellat reiciendis at quis labore, ducimus alias quos, quae quo sed, neque pariatur?
                         </div>
                     </div>
-                    <div className="col-md-3 my-2">
+                    <div className="col-md-3 mb-2">
                         <div className="bg-info p-3">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur quia dolor temporibus expedita. Amet, minima dolor. Rem repellat reiciendis at quis labore, ducimus alias quos, quae quo sed, neque pariatur?
                         </div>
                     </div>
-                    <div className="col-md-3 my-2">
+                    <div className="col-md-3 mb-2">
                         <div className="bg-info p-3">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur quia dolor temporibus expedita. Amet, minima dolor. Rem repellat reiciendis at quis labore, ducimus alias quos, quae quo sed, neque pariatur?
                         </div>
                     </div>
-                    <div className="col-md-3 my-2">
+                    <div className="col-md-3">
                         <div className="bg-info p-3">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur quia dolor temporibus expedita. Amet, minima dolor. Rem repellat reiciendis at quis labore, ducimus alias quos, quae quo sed, neque pariatur?
                         </div>
