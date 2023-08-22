f088b1265cbb5fe76b455d2e3f6a0f5457bc1f5b
Kaushik Biswas <kbutsho@gmail.com>
2023-08-22 07:13:00 +0600

    adminLayout copyright text added

--- src/layouts/admin/AdminLayout.js
+++ src/layouts/admin/AdminLayout.js
@@ -53,107 +53,113 @@ const AdminLayout = ({ children }) => {
         toast.success('logout successfully!')
     };
     return (
-        <div className="d-flex align-items-stretch">
-            <nav id={styles.sidebar} className={active ? `${styles.active}` : ""}>
-                <ul className={`${styles.components} list-unstyled mb-5`} style={{ marginTop: "20px" }}>
-                    {
-                        active ?
-                            <span onClick={toggleSidebar} id={styles.sidebarCollapse}>
-                                <Image
-                                    style={{ cursor: "pointer" }}
-                                    src={rightArrow} height={24} width={24} alt="img" />
-                            </span> :
-                            <li style={{ height: "40px" }}>
-                            </li>
-                    }
-                    <li>
-                        <Link href="/admin/dashboard" className='fw-bold'>
-                            <span className={styles.icon}><AiFillHome /></span>
-                            <span className={styles.link_text}>Dashboard</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/profile" className='fw-bold'>
-                            <span className={styles.icon}><BiSolidUser /></span>
-                            <span className={styles.link_text}>Profile</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/products" className='fw-bold'>
-                            <span className={styles.icon}><BsBoxSeamFill /></span>
-                            <span className={styles.link_text}>Products</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/brands" className='fw-bold'>
-                            <span className={styles.icon}><MdOutlineBrandingWatermark /></span>
-                            <span className={styles.link_text}>Brands</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/categories" className='fw-bold'>
-                            <span className={styles.icon}><BiSolidCategory /></span>
-                            <span className={styles.link_text}>Categories</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/managers" className='fw-bold'>
-                            <span className={styles.icon}><FaUserTie /></span>
-                            <span className={styles.link_text}>Managers</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/sellers" className='fw-bold'>
-                            <span className={styles.icon}><FaHospitalUser /></span>
-                            <span className={styles.link_text}>Sellers</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/customers" className='fw-bold'>
-                            <span className={styles.icon}><FaUsers /></span>
-                            <span className={styles.link_text}>Customers</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/admin/settings" className='fw-bold'>
-                            <span className={styles.icon}><FiSettings /></span>
-                            <span className={styles.link_text}>Settings</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <a onClick={handleLogout} className={` fw-bold`} style={{ cursor: "pointer" }}>
-                            <span className={styles.icon}><FaPowerOff color="red" /></span>
-                            <span className={styles.link_text}>Logout</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-            <div>
-                <nav className="navbar navbar-expand-lg" style={{ background: "#0A2647" }}>
-                    <div className='container'>
+        <div>
+            <div className="d-flex align-items-stretch">
+                <nav id={styles.sidebar} className={active ? `${styles.active}` : ""}>
+                    <ul className={`${styles.components} list-unstyled mb-5`} style={{ marginTop: "20px" }}>
                         {
-                            active ? null :
+                            active ?
                                 <span onClick={toggleSidebar} id={styles.sidebarCollapse}>
                                     <Image
                                         style={{ cursor: "pointer" }}
-                                        src={leftArrow} height={24} width={24} alt="img" />
-                                </span>
+                                        src={rightArrow} height={24} width={24} alt="img" />
+                                </span> :
+                                <li style={{ height: "40px" }}>
+                                </li>
                         }
-                        <ul className="nav navbar-nav ms-auto">
-                            <li>
-                                <Link className="nav-link d-flex align-items-center" href="/">
-                                    <BiSolidUserCircle size="30" color="white" />
-                                    <span className='ms-2 fw-bold text-white'>kbutsho</span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                        <li>
+                            <Link href="/admin/dashboard" className='fw-bold'>
+                                <span className={styles.icon}><AiFillHome /></span>
+                                <span className={styles.link_text}>Dashboard</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/profile" className='fw-bold'>
+                                <span className={styles.icon}><BiSolidUser /></span>
+                                <span className={styles.link_text}>Profile</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/products" className='fw-bold'>
+                                <span className={styles.icon}><BsBoxSeamFill /></span>
+                                <span className={styles.link_text}>Products</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/brands" className='fw-bold'>
+                                <span className={styles.icon}><MdOutlineBrandingWatermark /></span>
+                                <span className={styles.link_text}>Brands</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/categories" className='fw-bold'>
+                                <span className={styles.icon}><BiSolidCategory /></span>
+                                <span className={styles.link_text}>Categories</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/managers" className='fw-bold'>
+                                <span className={styles.icon}><FaUserTie /></span>
+                                <span className={styles.link_text}>Managers</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/sellers" className='fw-bold'>
+                                <span className={styles.icon}><FaHospitalUser /></span>
+                                <span className={styles.link_text}>Sellers</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/customers" className='fw-bold'>
+                                <span className={styles.icon}><FaUsers /></span>
+                                <span className={styles.link_text}>Customers</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/admin/settings" className='fw-bold'>
+                                <span className={styles.icon}><FiSettings /></span>
+                                <span className={styles.link_text}>Settings</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <a onClick={handleLogout} className={` fw-bold`} style={{ cursor: "pointer" }}>
+                                <span className={styles.icon}><FaPowerOff color="red" /></span>
+                                <span className={styles.link_text}>Logout</span>
+                            </a>
+                        </li>
+                    </ul>
                 </nav>
 
-                <div className="px-2" onContextMenu={handleContextMenu}>
-                    {children}
+                <div>
+                    <nav className="navbar navbar-expand-lg" style={{ background: "#0A2647" }}>
+                        <div className='container'>
+                            {
+                                active ? null :
+                                    <span onClick={toggleSidebar} id={styles.sidebarCollapse}>
+                                        <Image
+                                            style={{ cursor: "pointer" }}
+                                            src={leftArrow} height={24} width={24} alt="img" />
+                                    </span>
+                            }
+                            <ul className="nav navbar-nav ms-auto">
+                                <li>
+                                    <Link className="nav-link d-flex align-items-center" href="/">
+                                        <BiSolidUserCircle size="30" color="white" />
+                                        <span className='ms-2 fw-bold text-white'>kbutsho</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <div className="px-2" onContextMenu={handleContextMenu}>
+                        {children}
+                    </div>
                 </div>
+            </div>
+            <div className="footer text-center py-3 fw-bold" style={{ background: "#0A2647", color: "white" }}>
+                <small style={{ fontSize: "13px" }}>copyright© {new Date().getFullYear()} || all rights reserved by
+                    <a style={{ textDecoration: "none", color: "white" }} target='_blank' href="https://kbutsho.vercel.app/"> kbutsho</a></small>
             </div>
         </div>
     );
