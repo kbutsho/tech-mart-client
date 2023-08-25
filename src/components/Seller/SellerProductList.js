d3333e563907b71c9f402b23ae210046bb592b25
Kaushik Biswas <kbutsho@gmail.com>
2023-08-25 22:03:34 +0600

    update

--- src/components/Seller/SellerProductList.js
+++ src/components/Seller/SellerProductList.js
@@ -5,6 +5,8 @@ import { config } from "@/config";
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { Table } from 'react-bootstrap';
+import { FadeLoader } from 'react-spinners';
+import { AiFillDelete, AiFillEdit, AiFillEye } from 'react-icons/ai';
 
 const SellerProductList = () => {
     const [data, setData] = useState();
@@ -41,7 +43,7 @@ const SellerProductList = () => {
                 </div>
                 <div className="list-area">
                     {
-                        loading ? <div>loading...</div>
+                        loading ? <div className='d-flex justify-content-center align-items-center border mt-3' style={{ height: "50vh" }}><FadeLoader /></div>
                             :
                             <div className='p-3 border mt-3 table-area'>
                                 <Table striped hover responsive>
@@ -52,32 +54,38 @@ const SellerProductList = () => {
                                             <th>NAME</th>
                                             <th>BRAND</th>
                                             <th>CATEGORY</th>
-                                            <th>QUANTITY</th>
-                                            <th>PRICE</th>
-                                            <th>RATING</th>
-                                            <th>STATUS</th>
+                                            <th className='text-center'>STOCK</th>
+                                            <th className='text-center'>PRICE</th>
+                                            <th className='text-center'>RATING</th>
+                                            <th className='text-center'>SELL</th>
+                                            <th className='text-center'>STATUS</th>
                                             <th className='text-center'>ACTION</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {
-                                            data?.slice(0, 5).map((data, index) =>
-                                                <tr key={index}>
-                                                    <td>{index + 1}</td>
-                                                    <td>{data.code}</td>
-                                                    <td>{data.name}</td>
-                                                    <td>{data.brand}</td>
-                                                    <td>{data.category}</td>
-                                                    <td className='text-center'>{data.quantity}</td>
-                                                    <td>{data.price}</td>
-                                                    <td>{data.rating}</td>
-                                                    <td>{data.status}</td>
-                                                    <td className='d-flex justify-content-center'>
-                                                        <button className='btn btn-primary btn-sm mx-1'>Details</button>
-                                                        <button className='btn btn-success btn-sm mx-1'>Update</button>
-                                                        <button className='btn btn-danger btn-sm mx-1'>Delete</button>
-                                                    </td>
-                                                </tr>
+                                            data?.slice(0, 50).map((data, index) => {
+                                                return (
+                                                    <tr key={index}>
+                                                        <td className='fw-bold'>{(index + 1).toString().padStart(2, '0')}</td>
+                                                        <td>{data.code}</td>
+                                                        <td>{data.name}</td>
+                                                        <td>{data.brand}</td>
+                                                        <td>{data.category}</td>
+                                                        <td className='text-center'>{data.quantity}</td>
+                                                        <td className='text-center'>{data.price}</td>
+                                                        <td className='text-center'>{data.rating}</td>
+                                                        <td className='text-center'>{data.sellCount ?? 0}</td>
+                                                        <td className='text-center'>{data.status.split('-').join(' ')}</td>
+                                                        <td className='d-flex justify-content-center'>
+                                                            <button className='btn btn-primary btn-sm mx-1'><AiFillEye /></button>
+                                                            <button className='btn btn-success btn-sm mx-1'><AiFillEdit /></button>
+                                                            <button className='btn btn-danger btn-sm mx-1'><AiFillDelete /></button>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            }
+
                                             )
                                         }
                                     </tbody>
