fdf645a88ee2c4cb42271e450d976d17e8c7fc52
Kaushik Biswas <kbutsho@gmail.com>
2023-10-20 18:35:29 +0600

    added coming soon!

--- src/pages/admin/brands/index.js
+++ src/pages/admin/brands/index.js
@@ -10,6 +10,7 @@ const AdminBrands = () => {
             </Head>
             <Breadcrumb />
             <div className="px-md-3 px-2 py-2">
+                <h5 className="fw-bold text-danger">coming soon!</h5>
                 <p className="small-text">Tech Mart is an online tech store offering a wide range of cutting-edge electronics, gadgets, and accessories. It provides a user-friendly platform for customers to explore and purchase the latest tech products across various categories, ensuring a convenient and up-to-date shopping experience.</p>
             </div>
         </div>
--- src/pages/customer/dashboard/index.js
+++ src/pages/customer/dashboard/index.js
@@ -15,6 +15,7 @@ const CustomerDashboard = () => {
             </Head>
             <Breadcrumb />
             <div className="px-md-3 px-2 py-2">
+                <h5 className="fw-bold text-danger">coming soon!</h5>
                 <p className="small-text">Tech Mart is an online tech store offering a wide range of cutting-edge electronics, gadgets, and accessories. It provides a user-friendly platform for customers to explore and purchase the latest tech products across various categories, ensuring a convenient and up-to-date shopping experience.</p>
             </div>
         </div>
