ff135511df74bf216dc7823936f67557734197d6
Kaushik Biswas <kbutsho@gmail.com>
2024-04-05 02:48:52 +0600

    update

--- src/pages/seller/products/add/index.js
+++ src/pages/seller/products/add/index.js
@@ -14,10 +14,15 @@ const SellerAddProduct = ({ data }) => {
                 <title>add products</title>
             </Head>
             <Breadcrumb />
-            <div className="px-md-3 px-2 py-2">
+            <div className="px-md-3 px-2 py-2"
+                style={{
+                    height: "83vh",
+                    overflowY: "auto",
+                    scrollbarWidth: "none"
+                }}>
                 <AddSellerProduct categories={categories.data} brands={brands.data} />
             </div>
-        </div>
+        </div >
     );
 };
 
